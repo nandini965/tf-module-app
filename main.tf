15660ddeab11d57c9d15304f50a1d2bc3674b0f1
nandini965 <nandhini965@gmail.com>
2023-08-29 02:31:55 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -44,11 +44,12 @@ resource "aws_autoscaling_group" "asg" {
   max_size            = var.max_size
   min_size            = var.min_size
   vpc_zone_identifier = var.subnet_ids
-}
+
 
 
 launch_template {
   id      = aws_launch_template.template.id
   version = "$Latest"
-}
+  }
 
+}
