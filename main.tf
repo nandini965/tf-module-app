bba5cc7f7ba7b46c90781527663591b0569ae8c7
nandini965 <nandhini965@gmail.com>
2023-08-28 23:43:53 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -1,21 +1,4 @@
 
-resource "aws_launch_template" "template" {
-  name_prefix     = "${var.name} - ${var.env}-lt"
-  image_id        = data.aws_ami.ami.id
-  instance_type   = var.instance_type
- vpc_security_group_ids = [aws_security_group.sg.id]
-}
-resource "aws_autoscaling_group" "asg" {
-  name_prefix     = "${var.name} - ${var.env}-asg"
-  min_size             = var.min_size
-  max_size             = var.max_size
-  desired_capacity     = var.desired_capacity
-  vpc_zone_identifier  = var.subnet_ids
-  launch_template {
-    id = aws_launch_template.template.id
-    version = "$latest"
-  }
-}
 resource "aws_security_group" "sg" {
   name        = "${var.name}-${var.env}-sg"
   description = "${var.name}-${var.env}-sg"
@@ -45,5 +28,23 @@ resource "aws_security_group" "sg" {
 
   tags = {
     Name = "${var.name} - ${var.env}-sg"
+  }
+}
+resource "aws_launch_template" "template" {
+  name_prefix     = "${var.name} - ${var.env}-lt"
+  image_id        = data.aws_ami.ami.id
+  instance_type   = var.instance_type
+  vpc_security_group_ids = [aws_security_group.sg.id]
+}
+
+resource "aws_autoscaling_group" "asg" {
+  name_prefix     = "${var.name} - ${var.env}-asg"
+  min_size             = var.min_size
+  max_size             = var.max_size
+  desired_capacity     = var.desired_capacity
+  vpc_zone_identifier  = var.subnet_ids
+  launch_template {
+    id = aws_launch_template.template.id
+    version = "$latest"
   }
 }
