0152147f1b7e95e16437f067158fc699930f79c4
nandini965 <nandhini965@gmail.com>
2023-08-29 00:06:20 +0530

    filebeat

--- data.tf
+++ data.tf
@@ -1,5 +1,5 @@
-  data "aws_ami" "ami" {
+data "aws_ami" "ami" {
   most_recent = true
-  name_regex = "centos-8-devops-practice"
-  owners     =  ["973714476881"]
+  name_regex  = "centos-8-devops-practice"
+  owners      =  ["973714476881"]
 }
