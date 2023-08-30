ac736da32c3f0c06171baaa835622a01356de6dc
nandini965 <nandhini965@gmail.com>
2023-08-30 18:22:16 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 resource "aws_security_group" "sg" {
   name        = "${var.name}-${var.env}-sg"
   description = "${var.name}-${var.env}-sg"
-  vpc_id      = var.vpc_id
+  vpc_id      = var.vpc_ids
 
   ingress {
     description      = "app"
