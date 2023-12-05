e8daa683639097d4b35c4c66db0c6e61c00ea667
nandini965 <nandhini965@gmail.com>
2023-12-05 16:58:37 +0530

    filebeat

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 labauto ansible
-ansible-pull -i localhost, -U https://github.com/nandini965/roboshop-ansible roboshop.yml -e role_name=${name}  -e env=${env}
- &>>/opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/nandini965/roboshop-ansible roboshop.yml -e role_name=${name}  -e env=${env} &>>/opt/ansible.log
