cc92dd48a67cc8735cc84aa0d2c1ea352b1c3542
nandini965 <nandhini965@gmail.com>
2023-12-19 03:31:35 +0530

    filebeat

--- locals.tf
+++ locals.tf
@@ -7,5 +7,4 @@ locals {
   parameters = concat(var.parameters, [var.name])
   resources  = [for parameter in local.parameters : "arn:aws:ssm:us-east-1:${data.aws_caller_identity.identity.account_id}:parameter/${var.env}.${parameter}.*"]
 }
-//  resources  = [for parameter in var.parameters : "arn:aws:ssm:us-east-1:${data.aws_caller_identity.identity.account_id}:parameter/${var.env}.${var.name}.*"]
-//}
+
